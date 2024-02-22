c5650a437a02cc9f7b3fda971ae67e71ff512795
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2024-02-21 16:39:59 -0800

    Identifier shows that this is a active class

--- design-system.js
+++ design-system.js
@@ -1,10 +1,10 @@
+// JavaScript code for toggling the sidebar visibility
 function toggleSidebar() {
-    var sidebar = document.getElementById('side-bar');
-    sidebar.classList.toggle('active');
-  }
+  var sidebar = document.getElementById('side-bar');
+  sidebar.classList.toggle('active');
+}
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var iconButton = document.querySelector('.icon-button');
-    iconButton.addEventListener('click', toggleSidebar);
-  });
-  
+document.addEventListener('DOMContentLoaded', function() {
+  var iconButton = document.querySelector('.icon-button');
+  iconButton.addEventListener('click', toggleSidebar);
+});
