20757e83944abc3a721ed4205b0df0ee44b838f6
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2024-02-24 21:15:58 -0800

    Add a namespace

--- design-system.js
+++ design-system.js
@@ -1,10 +1,16 @@
 // JavaScript code for toggling the sidebar visibility
 function toggleSidebar() {
-  var sidebar = document.getElementById('side-bar');
+  var sidebar = document.getElementById('c-side-bar');
+  var check = document.getElementById('check');
+  // Toggle the "active" class on the sidebar
   sidebar.classList.toggle('active');
+
+  // Optional: Also toggle the "checked" state of the checkbox if needed
+  check.checked = !check.checked;
 }
 
+// Add the event listener to the hamburger icon
 document.addEventListener('DOMContentLoaded', function() {
-  var iconButton = document.querySelector('.icon-button');
+  var iconButton = document.querySelector('.c-icon-button');
   iconButton.addEventListener('click', toggleSidebar);
 });
