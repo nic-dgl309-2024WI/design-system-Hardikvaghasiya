cfaf108ae2b7c2aa45dae64a434a19326039ed39
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2024-03-16 21:46:02 -0700

    Update Mobile Navbar

--- design-system.js
+++ design-system.js
@@ -1,6 +1,6 @@
 // JavaScript code for toggling the sidebar visibility
 function toggleSidebar() {
-  var sidebar = document.getElementById('c-side-bar');
+  var sidebar = document.getElementById('c-side__sidebar'); // Corrected ID to match HTML
   var check = document.getElementById('check');
   // Toggle the "active" class on the sidebar
   sidebar.classList.toggle('active');
@@ -11,6 +11,6 @@ function toggleSidebar() {
 
 // Add the event listener to the hamburger icon
 document.addEventListener('DOMContentLoaded', function() {
-  var iconButton = document.querySelector('.c-icon-button');
+  var iconButton = document.querySelector('.c-c-navigation__icon-button'); // Corrected class name to match HTML
   iconButton.addEventListener('click', toggleSidebar);
 });
