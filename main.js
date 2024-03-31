07f9e001534f576b3722115bc0c0bc6f50de47ff
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2024-03-30 18:22:35 -0700

    Added Hero section with CTA and Image

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroContent = document.querySelector('.hero__content');
+    heroContent.style.opacity = 0;
+    window.setTimeout(function() {
+        heroContent.style.transition = 'opacity 2s';
+        heroContent.style.opacity = 1;
+    }, 500);
+});
